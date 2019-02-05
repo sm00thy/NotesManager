9a6e070d0443e4e6323c1f5f56a14931fc711bad
Filip Lipowiecki <lipowiecki.f@gmail.com>
2019-02-05 23:15:43 +0100

    navigation

--- src/NotesManager/Layouts/NotePage.xaml.cs
+++ src/NotesManager/Layouts/NotePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NotesManager.DataModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,25 @@ namespace NotesManager.Layouts
     /// </summary>
     public partial class NotePage : Page
     {
-        public NotePage()
+        public int NoteId { get; set; }
+        public int UserId { get; set; }
+
+        public NotePage(int noteId, int userId)
         {
             InitializeComponent();
+            NoteId = noteId;
+            UserId = userId;
+        }
+
+
+        private /*async Task*/ void SaveNoteBtnClick(object sender, RoutedEventArgs e)
+        {
+            var note = new Note(NoteId, noteTitle.Text, noteContent.Text);
+            if (note.Id == 0) ;
+            //strzał do bazy z createNote
+            else;
+            //strzał do bazy z updateNote
+            NavigationService.Navigate(new NoteList(UserId));
         }
     }
 }
